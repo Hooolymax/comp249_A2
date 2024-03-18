d9d3b92d0552a99c784ea1411b9021ba8fef3a3a
Alisa <alisa.a.ign@gmail.com>
2024-03-18 18:51:35 -0400

    upd2

--- Driver/Driver.java
+++ Driver/Driver.java
@@ -11,6 +11,8 @@ public class Driver {
         // part 1’s manifest file
         String part1_manifest = "part1_manifest.txt";
 
+
+       
         // part 2’s manifest file
         String part2_manifest = do_part1(part1_manifest /* , ... */); // partition
 
@@ -24,7 +26,24 @@ public class Driver {
     }
 
     public static String do_part1(String s) {
-        return "";
+
+
+        // read p1 manifest file line by line and handle the exception if it does not exist
+        // check if it s empty 
+        // if nor read it line by line and open the files containing movies (separate function)
+        
+        // handle the exceptions and go to the next file - 
+        // comment what is missing in case of exception(s) and put it into bad files
+        // based on a genre write it to a file 
+
+
+        // throws syntax and semantic exceptions
+        // function takes a file name, than open it and read line by line
+        // each line is checked for syntax errors and a movie object is created (and returned) if no errors else exception
+
+        // create manifest file
+
+        return ""; // returns name of part 2 manifest file
 
     }
 
