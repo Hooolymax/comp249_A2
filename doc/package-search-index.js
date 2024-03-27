a9c2a250ec3ebfd92c6529978b1f7b302e26d176
Hooolymax <volitantandre@gmail.com>
2024-03-27 19:52:43 -0400

    javaDoc

--- doc/package-search-index.js
+++ doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"driver"},{"l":"movie"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"driver"},{"l":"exceptions"},{"l":"movie"}];updateSearchResults();
